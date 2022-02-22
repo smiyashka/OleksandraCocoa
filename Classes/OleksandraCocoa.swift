2e513a40d9491a6190cc717e4b7478813c3c4d76
smiyashka <asmiyashka1@gmail.com>
2022-02-22 21:05:00 +0200

    add MyCode

---
+++ Classes/OleksandraCocoa.swift
@@ -0,0 +1,18 @@
+//
+//  OleksandraCocoa.swift
+//  Nimble
+//
+//  Created by Olexandra on 22.02.2022.
+//
+
+import Foundation
+ 
+public struct OleksandraCocoa {
+    
+    public init() {}
+    
+    public func sayCocoa() {
+        print("Hello Cocoa")
+    }
+    
+}
